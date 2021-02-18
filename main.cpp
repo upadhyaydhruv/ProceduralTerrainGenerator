f8d34226209282d77c48c34ca6b281a68673c44e
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-02-18 18:30:26 -0500

    added some post-rendering functions for opengl

--- main.cpp
+++ main.cpp
@@ -16,20 +16,48 @@ void initializeTerrain() {
     for (int x = 0; x < 1024; x++){
         for (int y = 0; y < 1024; y++) {
             heightMap[x][y] = generator.noise(x, y, generator.zValuesInsertion.at(x)) * MAP_SCALE; // multiplies by the preset amplitude
-        }
 
     }
 }
 
-// This function renders the heightmap into a terrain
+// This function renders the heightmap into a terrain using openGL
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity(); // Replaces the current matrix, which represents how we view a screen, with the 4X4 identity matrix
     glBindTexture(GL_TEXTURE_2D, land);
 
+    for (int x = 0; x < 1024; x++) {
+        // By initializing as GL_TRIANGLE_STRIP, each vertex after the first two will be dealt with as a triangle
+        // This needs to be done for every row in the heightmap
+        glBegin(GL_TRIANGLE_STRIP);
+
+        for (y = 0; y < 1024; y++) {
+
+            // Draw vertex 1
+            glColor3f(y, y, y);
+            gltexCoord2f(0.0f, 0.0f);
+            glVertex3f(x, y, z);
+
+        }
+    }
+
+    // Enables the blending between created triangles
+    glEnable(GL_BLEND);
 
 
+    glDepthMask(GL_FALSE);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+    glDepthMask(GL_TRUE);
+    glDisable(GL_BLEND);
+
+    glFlush();
+
+    SwapBuffers(g_HDC);
+
+    }
 }
 
 
