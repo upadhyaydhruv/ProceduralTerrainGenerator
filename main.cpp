3fb632304bc03da0656030ba89ad208c94dac219
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-02-17 21:53:41 -0500

    started some rendering with openGL

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <PerlinNoise.h>
 
-constexpr int MAP_SCALE = 4; // arbitrarily set, can be changed by the user whenever
+constexpr int MAP_SCALE = 50; // arbitrarily set, can be changed by the user whenever
 
 // Initialize the heightmap using the insertion values as values for z, which are passed into the noise function
 
@@ -15,9 +15,21 @@ void initializeTerrain() {
     int heightMap[1024][1024];
     for (int x = 0; x < 1024; x++){
         for (int y = 0; y < 1024; y++) {
-            heightMap[x][y] = generator.noise(x, y, generator.zValuesInsertion.at(x));
+            heightMap[x][y] = generator.noise(x, y, generator.zValuesInsertion.at(x)) * MAP_SCALE; // multiplies by the preset amplitude
         }
+
     }
+}
+
+// This function renders the heightmap into a terrain
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity(); // Replaces the current matrix, which represents how we view a screen, with the 4X4 identity matrix
+    glBindTexture(GL_TEXTURE_2D, land);
+
+
+
 }
 
 
@@ -35,6 +47,10 @@ void displayMe(void)
 }
 
 int main(int argc, char** argv) {
+
+    initializeTerrain();
+
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(400, 300);
